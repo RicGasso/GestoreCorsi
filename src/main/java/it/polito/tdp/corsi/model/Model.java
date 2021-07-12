0ff890f5a4fa5940f65adcedd945982188832b6d
Riccardo Gasso <s245627@studenti.polito.it>
2021-07-12 11:48:08 +0200

    esercizio finito GestoreCorsi

--- src/main/java/it/polito/tdp/corsi/model/Model.java
+++ src/main/java/it/polito/tdp/corsi/model/Model.java
@@ -1,5 +1,6 @@
 package it.polito.tdp.corsi.model;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -18,6 +19,39 @@ public class Model {
 	
 	public Map<Corso, Integer> getIscrittiByPeriodo(Integer pd){
 		return corsoDao.getIscrittibyPeriodo(pd);
+	}
+	
+	public List<Studente> getStudenteByCorso(String codice){
+		return corsoDao.getStudentiByCorso(new Corso(codice, null, null, null));
+	}
+	
+	public Map<String, Integer> getDivisioneCDS(String codice) {
+		//cosa ci aspettiamo 
+		//dato il corso con codice ABC
+		//gest -> 50
+		//inf -> 40
+		//mec -> 30
+		
+		//soluzione 1
+		/*
+		Map<String, Integer> divisione = new HashMap<String, Integer>();
+		List<Studente> studenti = this.getStudenteByCorso(codice);
+		for(Studente s:studenti) {
+			if(divisione.get(s.getCDS())==null) {
+				divisione.put(s.getCDS(), 1);
+			} else {
+				divisione.put(s.getCDS(), divisione.get(s.getCDS())+1);
+			}
+		}*/
+		
+		
+		
+		return corsoDao.getDivisioneStudenti(new Corso(codice, null, null, null));
+		
+	}
+
+	public boolean esisteCorso(String codice) {
+		return corsoDao.esisteCorso(new Corso(codice, null, null, null));
 	}
 	
 }
